40209185fef4ed7c559883c6d3705bfb56c88758
Sean Scott <sean@sscottdev.com>
2022-07-12 19:39:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+const output = document.getElementById("output")
+
+
+
+function add(){
+    output.innerHTML = `${parseInt(input1.value) + +input2.value}`
+}
+
+function subtract(){
+    output.innerHTML = `${parseInt(input1.value) - input2.value}`
+}
+
+function multi(){
+    output.innerHTML = `${parseInt(input1.value) * input2.value}`
+}
